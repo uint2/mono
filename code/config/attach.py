b20b3ac3f08ea08de336fb01af27309aea05bddf
nguyenvukhang <brew4k@gmail.com>
2026-06-08 03:35:44 -0400

    Download wallpaper

--- code/config/attach.py
+++ code/config/attach.py
@@ -1,4 +1,5 @@
 from os import path
+from urllib.request import urlretrieve
 from dataclasses import dataclass
 import os
 
@@ -34,9 +35,38 @@ links = [
 ]
 
 
-for i in range(len(links)):
-    v = links[i]
-    for u in links[:i]:
-        assert v.dst != u.dst, "Conflict in target."
-    assert path.exists(v.src), "Source of link should exist."
-    v.symlink()
+def link_dotfiles():
+    for i in range(len(links)):
+        v = links[i]
+        for u in links[:i]:
+            assert v.dst != u.dst, "Conflict in target."
+        assert path.exists(v.src), "Source of link should exist."
+    # Yes, TOCTOU, but come on. Doing the links after all these checks ensures
+    # that all checks pass before anything is done.
+    for link in links:
+        link.symlink()
+
+
+def photo_link(sha: str, file: str) -> str:
+    "Gets a download url from the uint2/photos repository."
+    return "/".join(("https://raw.githubusercontent.com/uint2/photos", sha, file))
+
+
+def download_files():
+    print("Downloading wallpaper...")
+    urlretrieve(
+        photo_link(
+            "9ea5fdbd460b3b77cd50bc432169446ce5d58a31",
+            "b78e47dd63166dd1ec381951defb3a633521457e.jpg",
+        ),
+        path.join(HOME, ".local/share/wall.jpg"),
+    )
+
+
+def main():
+    link_dotfiles()
+    download_files()
+
+
+if __name__ == "__main__":
+    main()
