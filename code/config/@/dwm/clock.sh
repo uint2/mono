d1b27668f89f3894b16225ce8f609dc6db350697
nguyenvukhang <brew4k@gmail.com>
2026-06-11 02:33:21 -0400

    Add singapore timezone to dwm clock

--- code/config/@/dwm/clock.sh
+++ code/config/@/dwm/clock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SG_DATE=$(date "+%a %b %d, %H:%M")
+SG_DATE=$(TZ=Asia/Singapore date "+%a %b %d, %H:%M")
 LN_DATE=$(TZ=Europe/London date "+%H:%M")
 
 DISPLAY=:0 xsetroot -name "($LN_DATE)  $SG_DATE"
