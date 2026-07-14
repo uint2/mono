61275359eef6292405ec09fd7f557beb583f3060
nguyenvukhang <brew4k@gmail.com>
2026-07-14 21:38:46 +0800

    An attempt was made to start git3

--- code/tools/git3/src/bin/gitnu.zig
+++ code/tools/git3/src/bin/gitnu.zig
@@ -1,34 +1,19 @@
 const std = @import("std");
+const log = std.log;
+
 const git3 = @import("git3");
 
 const logger = @import("monologue").init(.{ .print_colors = true });
 
 pub const std_options: std.Options = .{
-    .log_level = .err,
+    .log_level = .debug,
     .logFn = logger.logFn,
 };
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try git3.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    var cwdBuf: [1024]u8 = undefined;
+    const cwd = std.fs.cwd().realpath(".", &cwdBuf) catch |err| {
+        return std.debug.print("Failed to get current working directory.\n{any}", .{err});
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    log.info("cwd: {s}", .{cwd});
 }
