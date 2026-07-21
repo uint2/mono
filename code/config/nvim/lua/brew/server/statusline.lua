d0a98b832bce3b474f1c664f59ffc4c64f5a9411
nguyenvukhang <brew4k@gmail.com>
2026-07-21 21:27:40 +0800

    Add a lua function call within the statusline

--- code/config/nvim/lua/brew/server/statusline.lua
+++ code/config/nvim/lua/brew/server/statusline.lua
@@ -1,6 +1,12 @@
+function _G._statusline_filepath()
+  local fp = vim.fn.expand("%")
+  return fp
+end
+
 local set_line = function(branch)
   branch = '%#StatusLineBranch#' .. branch .. '%#StatusLine#'
-  vim.opt.statusline = ' %f %h%w%m%r ' .. branch .. '%=+ '
+  vim.opt.statusline = '  %{%v:lua._statusline_filepath()%} %h%w%m%r ' .. branch .. '%=+ '
+  -- see "h: statusline" for more info.
 end
 
 require('brew.git-branch').init(set_line)
