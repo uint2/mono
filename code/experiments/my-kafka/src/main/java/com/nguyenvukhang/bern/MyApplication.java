27ca61627dc222f2bf4c04846e7f9a453e6fb764
nguyenvukhang <brew4k@gmail.com>
2026-07-22 00:24:03 +0800

    Back on Kafka

--- code/experiments/my-kafka/src/main/java/com/nguyenvukhang/bern/MyApplication.java
+++ code/experiments/my-kafka/src/main/java/com/nguyenvukhang/bern/MyApplication.java
@@ -1,20 +1,26 @@
 package com.nguyenvukhang.bern;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 
-@RestController
 @SpringBootApplication
 public class MyApplication {
 
-  @RequestMapping("/")
-  String home() {
-    return "Hello World!";
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(MyApplication.class, args);
+  }
+
+  @Bean
+  NewTopic topic() {
+    return TopicBuilder.name("topic1").partitions(10).replicas(1).build();
+  }
+
+  @KafkaListener(id = "myId", topics = "topic1")
+  public void listen(String in) {
+    System.out.println(in);
   }
 }
