b71ad8404f44be8b2a4ed76b22534aed9c4725c6
nguyenvukhang <brew4k@gmail.com>
2026-07-24 23:09:31 +0800

    Shell-based run script for kopiwm

---
+++ code/tools/kopiwm/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+TIMEOUT=10
+
+TMPDIR=$(mktemp)
+
+cleanup() {
+  rm -rf $TMPDIR
+}
+trap cleanup EXIT INT
+
+smash() {
+  sleep $TIMEOUT
+  killall kopiwm
+}
+
+rm -rf $TMPDIR
+mkdir -p $TMPDIR
+
+echo 'picom --frame-opacity=1.0 --backend xrender &' > $TMPDIR/xinitrc
+echo 'feh --bg-fill ~/.local/share/wall.jpg &'      >> $TMPDIR/xinitrc
+echo 'exec ~/.local/bin/kopiwm'                     >> $TMPDIR/xinitrc
+
+cat $TMPDIR/xinitrc
+
+# smash &
+XINITRC=$TMPDIR/xinitrc startx -- -keeptty >stdout.log 2>stderr.log
