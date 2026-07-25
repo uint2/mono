21288a29816d6a344eddbf4b3d3094e82a2410a2
nguyenvukhang <brew4k@gmail.com>
2026-07-25 10:11:17 +0800

    Update configs

--- code/config/nvim/lua/brew/server/statusline.lua
+++ code/config/nvim/lua/brew/server/statusline.lua
@@ -1,11 +1,15 @@
 function _G._statusline_filepath()
-  local fp = vim.fn.expand("%")
-  return fp
+  local fp = vim.fn.expand('%')
+  local non_empty = fp ~= nil and #fp > 0
+  if non_empty then return fp end
+  return ''
 end
 
 local set_line = function(branch)
   branch = '%#StatusLineBranch#' .. branch .. '%#StatusLine#'
-  vim.opt.statusline = '  %{%v:lua._statusline_filepath()%} %h%w%m%r ' .. branch .. '%=+ '
+  vim.opt.statusline = '%{%v:lua._statusline_filepath()%} %h%w%m%r '
+    .. branch
+    .. '%=+ '
   -- see "h: statusline" for more info.
 end
 
